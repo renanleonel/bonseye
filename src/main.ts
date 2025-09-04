228dd3bdaabfa89020d40eed436a8a012c71b6e1
renanleonel <renanleonelpro@gmail.com>
2025-09-04 08:24:11 -0300

    chore: clear project

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
-import './assets/main.css'
+import '@/main.css'
 
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 
 import App from '@/App.vue'
 import router from '@/router'
+import Aura from '@primeuix/themes/aura'
 import PrimeVue from 'primevue/config'
 
 const app = createApp(App)
 
 app.use(router)
-app.use(PrimeVue)
 app.use(createPinia())
+app.use(PrimeVue, { theme: { preset: Aura } })
 
 app.mount('#app')
