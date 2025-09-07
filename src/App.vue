a2ca261b4fc2238ebbacb44a309e88114deec1b8
renanleonel <renanleonelpro@gmail.com>
2025-09-07 09:55:16 -0300

    feat: add vercel analytics

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import Header from '@/components/header/HeaderMenu.vue'
+import { Analytics } from '@vercel/analytics/vue'
 import { RouterView } from 'vue-router'
 </script>
 
 <template>
+  <Analytics />
+
   <div class="bg-[#F7F6F5] min-h-screen h-full">
     <Header />
     <main id="app" class="max-w-6xl mx-auto">
